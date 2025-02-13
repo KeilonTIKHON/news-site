ed9907b46674490141dfa2c0adbd50a662447dba
KeilonTIKHON <veryovka2003@gmail.com>
2025-02-13 17:55:30 +0300

    module 1-4 done

---
+++ news-site/src/app/lib/contentful.js
@@ -0,0 +1,7 @@
+const contentful = require('contentful')
+
+export const client = contentful.createClient({
+  space: `${process.env.NEWS_SITE_SPACE_ID}`,
+  environment: "master", // defaults to 'master' if not set
+  accessToken: `${process.env.NEWS_SITE_CONTENT_DELIVERY_KEY}`
+})
